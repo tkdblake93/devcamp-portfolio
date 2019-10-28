15f46d6b2e3a6432466cffe57f7671c8143fd3cc
Blake Hutchinson <tkdblake93@yahoo.com>
2019-10-27 20:19:02 -0500

    Added toggle capability to set blog items to draft and publish.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,24 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
 	get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'pages/home'
+	get 'about-me', to: 'pages#about'
+	get 'contact', to: 'pages#contact'
 
-  get 'pages/about'
+	resources :blogs do
+		member do
+			get :toggle_status
+		end
+	end
 
-  get 'pages/contact'
-
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: 'pages#home'
 end
+
+  # get 'pages/home'
+
+  # get 'pages/about'
+
+  # get 'pages/contact'
+
+  # resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
